8047e21d9a947add6b494487624c224781b53436
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-10-15 21:49:13 +0100

    ff

---
+++ SecuraBox/SecuraApp/Views/v1_views.py
@@ -0,0 +1,217 @@
+
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework import generics
+from rest_framework_simplejwt.tokens import RefreshToken
+from SecuraApp.models.v1_models import *
+from rest_framework.response import Response
+from rest_framework import status
+from SecuraApp.Serializer.v1_serializer import *
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+
+
+
+
+
+
+
+# CREATE ACCOUNT
+class UsersRegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = CustomUserRegisterSerializer
+
+    def create(self, request, *args, **Kwargs):                                    
+        serializer = self.serializer_class(data= request.data)
+        if serializer.is_valid(raise_exception= True):
+            serializer.save()
+            user = serializer.data
+            return Response({'data': user}, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+class createPinView(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = createPinSerializer 
+    
+    def create(self, request, *args, **kwargs):
+        serializer= self.get_serializer(data= request.data)
+        if serializer.is_valid(raise_exception = True):
+            serializer.save()
+            return Response({'message': 'Pin Set Sucessfully'}, status= status.HTTP_201_CREATED)
+        return Response({'message': 'Authentication required'}, status= status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+# LOGIN ACCOUNT
+class UserLoginViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = CustomuserLoginSerialzer
+
+    def create(self, request):
+        serializer = self.serializer_class(data = request.data)
+        if serializer.is_valid(raise_exception= True):
+           user = serializer.validated_data['user']
+           refresh = RefreshToken.for_user(user)
+           return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'message': 'Logged in successfully'
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+class SocialmediaViewset(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    permission_classes = [IsAuthenticated]
+    serializer_class = SocialmediaSerializer
+    queryset = SocialMedia.objects.all()
+
+    def create(self, request):
+        user = request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializer = self.serializer_class(data = request.data)
+            serializer.is_valid(raise_exception = True)
+            serializer.save(user_id = user.id)
+            return Response({'message': 'Created Suceessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'not a valid user'}, status= status.HTTP_400_BAD_REQUEST)      
+
+
+
+
+
+
+class MailViewset(viewsets.ModelViewSet):
+    serializer_class = MailSerializer
+    queryset = Mail.objects.all()
+
+    def create(self, request):
+        user = request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializer = self.serializer_class(data = request.data)
+            serializer.is_valid(raise_exception = True)
+            serializer.save(user_id = user.id)
+            return Response({'message': 'Created Suceessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'not a valid user'}, status= status.HTTP_400_BAD_REQUEST)
+    
+
+
+class OnlineBankViewset(viewsets.ModelViewSet):
+    serializer_class = OnlineBankSerializer
+    queryset = OnlineBanking.objects.all()
+
+    def create(self, request):
+        user = request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializer = self.serializer_class(data = request.data)
+            serializer.is_valid(raise_exception= True)
+            serializer.save(user_id = user.id)
+            return Response({'message': 'Created Suceessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'not a valid user'}, status= status.HTTP_400_BAD_REQUEST)
+
+
+
+class CreditCardViewset(viewsets.ModelViewSet):
+    serializer_class = CreditCardSerializer
+    queryset = CreditCard.objects.all()
+
+    def create(self, request):
+        user = request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializers = self.serializer_class(data = request.data)
+            serializers.is_valid(raise_exception= True)
+            serializers.save(user = user)
+            return Response({'message': 'Created Sucessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'not a valid user'}, status= status.HTTP_400_BAD_REQUEST)
+        
+
+
+class NationalIDViewset(viewsets.ModelViewSet):
+    serializer_class = NationalIDSerializer
+    queryset = NationalID.objects.all()
+
+    def create(self, request):
+        user = request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializers = self.serializer_class(data = request.data)
+            serializers.is_valid()
+            serializers.save(user = user)
+            return Response({'message': 'Created Sucessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Not a Valid User'}, status= status.HTTP_400_BAD_REQUEST)
+        
+
+
+class DriverLicenseViewset(viewsets.ModelViewSet):
+    serializer_class = DriversLicenseSerializer
+    queryset = DriversLicense.objects.all()
+
+    def create(self, request):
+        user = request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializers = self.serializer_class(data = request.data)
+            serializers.is_valid(raise_exception= True)
+            serializers.save(user = user)
+            return Response({'message': 'Created Sucessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Not a Valid User'}, status= status.HTTP_400_BAD_REQUEST)
+        
+            
+
+class CertificateViewset(viewsets.ModelViewSet):
+    serializer_class = CertificateSerializer
+    queryset = Certificates.objects.all()
+
+    def create(self, request):
+        user = self.request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializers = self.serializer_class(data = request.data)
+            serializers.is_valid(raise_exception= True)
+            serializers.save(user = user)
+            return Response({'message': 'Created Sucessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Not a Valid User'}, status= status.HTTP_400_BAD_REQUEST)
+        
+
+class NoteViewset(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Notes.objects.all()
+
+    def create(self, request):
+        user = self.request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializers = self.serializer_class(data = request.data)
+            serializers.is_valid(raise_exception= True)
+            serializers.save(user = user)
+            return Response({'message': 'Created Sucessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Not a Valid User'}, status= status.HTTP_400_BAD_REQUEST)
+        
+
+
+class DocumentViewset(viewsets.ModelViewSet):
+    serializer_class = DocumentSerializer
+    queryset = Document.objects.all()
+
+    def create(self, request):
+        user = self.request.user
+        if user.is_authenticated or isinstance(user, CustomUser):
+            serializers = self.serializer_class(data = request.data)
+            serializers.is_valid(raise_exception= True)
+            serializers.save(user = user)
+            return Response({'message': 'Created Sucessfully'}, status= status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Not a Valid User'}, status= status.HTTP_400_BAD_REQUEST)
+        
---
+++ SecuraBox/SecuraBox/settings.py
@@ -0,0 +1,186 @@
+
+import os
+from pathlib import Path
+from datetime import timedelta
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-=pq94(yn#0$%!c@w6@#-xb_r12thhp3lj(lx8n1sf2bja2j1$%'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',       
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
+    'SecuraApp',
+    'django_cryptography',
+    # 'django_otp',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',   
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'SecuraBox.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'SecuraBox.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SecuraBox',
+        'USER': 'postgres',
+        'PASSWORD': 'Django@2024',
+        'HOST': 'localhost',
+        'PORT': '8000',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+MEDIA_URL = '/Media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'SecuraApp.CustomUser'
+
+
+
+FIELD_ENCRYPTION_KEY = '75171jc1WzKBkvXqEQQJ3I1yMAHXNt0RG-jWPYu27ds='
+
+
+
+
+REST_FRAMEWORK = {
+
+        'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    
+    ),
+    
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),  
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),    
+    'BLACKLIST_AFTER_ROTATION': True,
+
+}
+
+
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+SESSION_COOKIE_NAME = 'sessionid'
+SESSION_COOKIE_SECURE = False 
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'  
+EMAIL_HOST_PASSWORD = 'your_sendgrid_api_key' 
+DEFAULT_FROM_EMAIL = 'your_email@example.com'
+
+
+
