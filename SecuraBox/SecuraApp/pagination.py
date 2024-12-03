bda1fc2585ab0029314c33c5cfed5032ea564148
marvelousajuzie <Chizurummarvelous14@gmail.com>
2024-12-03 22:42:33 +0100

    jjj

---
+++ SecuraBox/SecuraApp/pagination.py
@@ -0,0 +1,10 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+
+
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 15  
+    page_size_query_param = 'page_size' 
+    max_page_size = 50 
--- SecuraBox/SecuraBox/settings.py
+++ SecuraBox/SecuraBox/settings.py
@@ -185,6 +185,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     
     ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 15,
     
 }
 
