398e8cf688ee49e33f4e5b8a125f27fa1eb182c2
marvelousajuzie <Chizurummarvelous14@gmail.com>
2025-01-02 13:09:37 +0100

    ddd

--- SecuraBox/SecuraBox/settings.py
+++ SecuraBox/SecuraBox/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from datetime import timedelta
+import os
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -36,7 +37,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
